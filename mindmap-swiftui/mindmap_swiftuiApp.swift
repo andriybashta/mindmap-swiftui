8a904685603d3f9ed0281c435279a050373af0be
Bashta <>
2021-11-14 03:01:07 +0200

    Init

---
+++ mindmap-swiftui/mindmap_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindmap_swiftuiApp.swift
+//  mindmap-swiftui
+//
+//  Created by Bashta on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct mindmap_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
